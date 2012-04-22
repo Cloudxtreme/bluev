849ed5b77c03f23e6ed81fcfe21924d6700fdede
tz <thomas@mich.com>
2012-04-21 22:21:23 -0400

    indent and cleanup source

--- bluev.c
+++ bluev.c
@@ -31,65 +31,61 @@ ISR(USART_RX_vect)
 {
     unsigned char c = UDR;
 
-    switch( inmsgstate ) {
+    switch (inmsgstate) {
     case 0:
-	if (c == 0xaa) {
-	    midcks = 0;
-	    midlen = 0;
-	    inmsgstate++;
-	}
-	break;
+        if (c == 0xaa) {
+            midcks = 0;
+            midlen = 0;
+            inmsgstate++;
+        }
+        break;
     case 1:
-	if ((c & 0xf0) == 0xd0)
-	    inmsgstate++;
-	else
-	    inmsgstate = 0;
-	break;
+        if ((c & 0xf0) == 0xd0)
+            inmsgstate++;
+        else
+            inmsgstate = 0;
+        break;
     case 2:
-	if ((c & 0xf0) == 0xe0 && (c & 0x0f) >= 3 && (c & 0x0f) <= 5) // valid source
-	    inmsgstate++;
-	else
-	    inmsgstate = 0;
-	break;
+        if ((c & 0xf0) == 0xe0 && (c & 0x0f) >= 3 && (c & 0x0f) <= 5)   // valid source
+            inmsgstate++;
+        else
+            inmsgstate = 0;
+        break;
     case 3:
-	if( midlen == 4 && c > 22 ) { // length
-	    inmsgstate = 0;
-	    break;
-	}
-	if( midlen < 5 )
-	    break;
-	if ( midbuf[4] + 4 == midlen) { // checksum byte
-	    if( midcks != c )
-		inmsgstate = 0;
-	    break;
-	}
+        if (midlen == 4 && c > 22) {    // length
+            inmsgstate = 0;
+            break;
+        }
+        if (midlen < 5)
+            break;
+        if (midbuf[4] + 4 == midlen) {  // checksum byte
+            if (midcks != c)
+                inmsgstate = 0;
+            break;
+        }
 
-	if ( c == 0xab && midbuf[4] + 5 == midlen) {
-	    midbuf[midlen] = c; // avoid race 
-	    slice = midbuf[2] & 0x0f;
-	    inmsgstate = 4;
-	}
-	break;
-    case 4: // waiting for transmit
-	return;
+        if (c == 0xab && midbuf[4] + 5 == midlen) {
+            midbuf[midlen] = c; // avoid race 
+            slice = midbuf[2] & 0x0f;
+            inmsgstate = 4;
+        }
+        break;
+    case 4:                    // waiting for transmit
+        return;
     default:
-	break;
+        break;
     }
-    if( inmsgstate && midlen < MIDSIZE ) {
-	midbuf[midlen++] = c;
-	midcks += c;
+    if (inmsgstate && midlen < MIDSIZE) {
+        midbuf[midlen++] = c;
+        midcks += c;
     }
     else
-	inmsgstate = 0;
+        inmsgstate = 0;
 }
 
 /*-------------------------------------------------------------------------*/
-static unsigned char polarity;
 //static unsigned int hitime;
-static unsigned int bitcnt;
 
-
-/*-------------------------------------------------------------------------*/
 #if 0
 ISR(TIMER1_OVF_vect)
 {
@@ -97,6 +93,7 @@ ISR(TIMER1_OVF_vect)
 }
 #endif
 
+static unsigned int bitcnt;
 // This counts character times to find the slot to transmit.  
 // Each slot is 45 character times wide
 // FIXME - doesn't try to resync or otherwise avoid collisions with other devices
@@ -105,88 +102,96 @@ ISR(TIMER1_COMPB_vect)
     OCR1B += BITTIME(10);
     frametime++;
 
-    if( frametime < 45*slice )
-	return;
+    if (frametime < 45 * slice)
+        return;
 
-    if( inmsgstate != 4 ) { // nothing for this frame
-	TIMSK &= ~_BV(OCIE1B);
-	UCSRB &= ~_BV(TXEN);      /* disable tx */
-	frametime = 0;
-	return;
+    if (inmsgstate != 4) {      // nothing for this frame
+        TIMSK &= ~_BV(OCIE1B);
+        UCSRB &= ~_BV(TXEN);    /* disable tx */
+        frametime = 0;
+        return;
     }
 
-    if( frametime == 45*slice ) {
-	UCSRB |= _BV(TXEN);      /* enable tx */
-	return;
+    if (frametime == 45 * slice) {
+#if 0
+        // Holdoff for late previous time slice
+        if (bitcnt)
+            frametime--;
+        else
+#endif
+            UCSRB |= _BV(TXEN); /* enable tx */
+        return;
     }
 
-    if( frametime >= 45*(slice+1) ) {
-	TIMSK &= ~_BV(OCIE1B);
-	UCSRB &= ~_BV(TXEN);      /* disable tx */
-	frametime = 0;
-	inmsgstate = 0;
-	return;
+    if (frametime >= 45 * (slice + 1)) {
+        TIMSK &= ~_BV(OCIE1B);
+        UCSRB &= ~_BV(TXEN);    /* disable tx */
+        frametime = 0;
+        inmsgstate = 0;
+        return;
     }
 
-    if( !(frametime & 1) ) { // Data Out Pacing, every other frame until done
-	unsigned char ptr = (frametime - (45*slice+1)) >> 1;
-	if( ptr < midlen )
-	    UDR = midbuf[ptr];
-	if( ptr > midlen )	
-	    UCSRB &= ~_BV(TXEN);      /* disable tx */
+    if (!(frametime & 1)) {     // Data Out Pacing, every other frame until done
+        unsigned char ptr = (frametime - (45 * slice + 1)) >> 1;
+        if (ptr < midlen)
+            UDR = midbuf[ptr];
+        if (ptr > midlen)
+            UCSRB &= ~_BV(TXEN);        /* disable tx */
     }
 }
 
 // This tracks the V1 infDisplayData packet to sync the ESP cycle
 static unsigned char v1state, thislen;
 const unsigned char infDisp[] = "\xaa\xd8\xea\x31\x09"; // put in Flash?
-void dostate(unsigned char val) {
+void dostate(unsigned char val)
+{
     // FIXME - hardcoded packet length
-    if( v1state < 5 ) {
-	if (val == infDisp[v1state] ) {
-	    v1state++;
-	    thislen = v1state;
-	}
-	else
-	    v1state = 0;
-	return;
+    if (v1state < 5) {
+        if (val == infDisp[v1state]) {
+            v1state++;
+            thislen = v1state;
+        }
+        else
+            v1state = 0;
+        return;
     }
     thislen++;
-    if( thislen == 11 && (val & 2) ) { // V1 TimeSlice holdoff
-	v1state = 0;
-	return;
+    if (thislen == 11 && (val & 2)) {   // V1 TimeSlice holdoff
+        v1state = 0;
+        return;
     }
 #if 0
     // FIXME? maybe check checksum v1 receives
-    if( thislen == 14 && val != ckcksum(inbuf, 14) )
-	return 0;
+    if (thislen == 14 && val != ckcksum(inbuf, 14))
+        return 0;
 #endif
-    if( val == 0xab && thislen == 15 ) {
-	frametime = 0;
-	v1state = 0;
-	OCR1B = OCR1A + BITTIME(10) + BITTIME(1) / 2;
-	TIFR |= _BV(OCF1B);         /* clear compare match interrupt */
-	TIMSK |= _BV(OCIE1B);       /* enable compare match interrupt */
+    if (val == 0xab && thislen == 15) {
+        frametime = 0;
+        v1state = 0;
+        OCR1B = OCR1A + BITTIME(10) + BITTIME(1) / 2;
+        TIFR |= _BV(OCF1B);     /* clear compare match interrupt */
+        TIMSK |= _BV(OCIE1B);   /* enable compare match interrupt */
     }
 }
 
 static unsigned char outchar;
+static unsigned char polarity;
 //Stopbit for software UART
 ISR(TIMER1_COMPA_vect)
 {
-    TIMSK &= ~_BV(OCIE1A); // disable
-    if( !polarity ) { // not break condition
-	while( bitcnt < 10 ) {
-	    bitcnt++;
-	    outchar >>= 1;
-	    outchar |= 0x80;
-	}
-	dostate(outchar);
-	//	UDR = 0x30 + thislen;
+    TIMSK &= ~_BV(OCIE1A);      // disable
+    if (!polarity) {            // not break condition
+        while (bitcnt < 10) {
+            bitcnt++;
+            outchar >>= 1;
+            outchar |= 0x80;
+        }
+        dostate(outchar);
+        //      UDR = 0x30 + thislen;
     }
-    else { // break, reset things for next start bit
-	TCCR1B &= ~_BV(ICES1);
-	polarity = 0;
+    else {                      // break, reset things for next start bit
+        TCCR1B &= ~_BV(ICES1);
+        polarity = 0;
     }
     bitcnt = 0;
 }
@@ -194,28 +199,28 @@ ISR(TIMER1_COMPA_vect)
 // Software UART via edges
 ISR(TIMER1_CAPT_vect)
 {
-static unsigned lastedge;
+    static unsigned lastedge;
     TCCR1B ^= _BV(ICES1);
     /* toggle interrupt on rising/falling edge */
-    if( !polarity && !bitcnt ) { // start bit
-	lastedge = ICR1;
-	OCR1A = lastedge + BITTIME(9) + BITTIME(1) / 2;
-	TIFR |= _BV(OCF1A);         /* clear compare match interrupt */
-	TIMSK |= _BV(OCIE1A);       /* enable compare match interrupt */
-	polarity = 1;
-	bitcnt = 1;
-	return;
+    if (!polarity && !bitcnt) { // start bit
+        lastedge = ICR1;
+        OCR1A = lastedge + BITTIME(9) + BITTIME(1) / 2;
+        TIFR |= _BV(OCF1A);     /* clear compare match interrupt */
+        TIMSK |= _BV(OCIE1A);   /* enable compare match interrupt */
+        polarity = 1;
+        bitcnt = 1;
+        return;
     }
     unsigned thisedge = ICR1;
     unsigned width = thisedge - lastedge;
     lastedge = thisedge;
-    width += BITTIME(1) / 2; // round up
-    while( width >= BITTIME(1) ) {
-	width -= BITTIME(1);
-	bitcnt++;
-	outchar >>= 1;
-	if( !polarity )
-	    outchar |= 0x80;
+    width += BITTIME(1) / 2;    // round up
+    while (width >= BITTIME(1)) {
+        width -= BITTIME(1);
+        bitcnt++;
+        outchar >>= 1;
+        if (!polarity)
+            outchar |= 0x80;
     }
     polarity ^= 1;
 }
@@ -229,10 +234,10 @@ int main(void)
     ACSR = _BV(ACD);            /* disable analog comparator */
 
     /* for testing
-    inmsgstate = 4;
-    strcpy_P( midbuf, PSTR("\xaa\xda\xe4\x41\x01\xaa\xab") );
-    midlen = 7;
-    */
+       inmsgstate = 4;
+       strcpy_P( midbuf, PSTR("\xaa\xda\xe4\x41\x01\xaa\xab") );
+       midlen = 7;
+     */
     v1state = inmsgstate = midlen = 0;
 
     /* setup serial port */
@@ -248,7 +253,7 @@ int main(void)
     UCSRB = _BV(RXEN) | _BV(RXCIE);
 
     GTCCR = _BV(PSR10);         /* reset prescaler */
-    TCNT1 = 0;         /* reset counter value */
+    TCNT1 = 0;                  /* reset counter value */
     // hitime = 0;
     /* activate noise canceller, */
     /* trigger on falling edge, clk/1 */
@@ -261,7 +266,7 @@ int main(void)
     TIFR |= _BV(ICF1) | _BV(TOV1) | _BV(OCF1A) | _BV(OCF1B);
     TIMSK |= _BV(ICIE1);
 
-    //TIMSK |= _BV(TOIE1); 	/* enable Overflow interrupt */
+    //TIMSK |= _BV(TOIE1);      /* enable Overflow interrupt */
 
     sei();
 
--- v1send.c
+++ v1send.c
@@ -8,55 +8,54 @@ unsigned char cmdbuf[24] = "\xaa\xda\xe0\x00\x01\x00\xab";
 unsigned char chmbuf[24] = "\xaa\xda\xe0\x36\x02\x00\x00\xab";
 
 unsigned char optable[] = { 0x00,
-			    0x01,0x03,0x11,0x16,0x18,0x19,0x22,0x32,0x33,0x34,0x35,0x41,0x42, 0x62, 0x71,0x73,0x36,0x36,0x36 };
+    0x01, 0x03, 0x11, 0x16, 0x18, 0x19, 0x22, 0x32, 0x33, 0x34, 0x35, 0x41, 0x42, 0x62, 0x71, 0x73, 0x36, 0x36, 0x36
+};
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int op = -1;
-    if( argc > 1 )
-	op = atoi( argv[1] );
-    if( op < 1 || op > sizeof(optable) ) {
-	printf( 
-"1 - Version\n"
-"2 - SerNo\n"
-"3 - UserBytes\n"
-"4 - SweepDefs\n"
-"5 - DefSweeps\n"
-"6 - MaxSwpIdx\n"
-"7 - SweepSects\n"
-"8 - MainDispOFF\n"
-"9 - MainDispON\n"
-"10 - MuteON\n"
-"11 - MuteOFF\n"
-"12 - AlertDatON\n"
-"13 - AlertDatOFF\n"
-"14 - BattVolt\n"
-"15 - SavvyStat\n"
-"16 - SavvyVehSPd\n"
-
-"17 - AllBogeys\n"
-"18 - Logic\n"
-"19 - AdvancedLogic\n"
-
-		);
-	return -1;
+    if (argc > 1)
+        op = atoi(argv[1]);
+    if (op < 1 || op > sizeof(optable)) {
+        printf(
+	       "1 - Version\n"
+	       "2 - SerNo\n"
+	       "3 - UserBytes\n"
+	       "4 - SweepDefs\n"
+	       "5 - DefSweeps\n"
+	       "6 - MaxSwpIdx\n"
+	       "7 - SweepSects\n"
+	       "8 - MainDispOFF\n"
+	       "9 - MainDispON\n"
+	       "10 - MuteON\n"
+	       "11 - MuteOFF\n"
+	       "12 - AlertDatON\n"
+	       "13 - AlertDatOFF\n"
+	       "14 - BattVolt\n" 
+	       "15 - SavvyStat\n" 
+	       "16 - SavvyVehSPd\n" 
+	       "17 - AllBogeys\n" 
+	       "18 - Logic\n" 
+	       "19 - AdvancedLogic\n"
+	       );
+        return -1;
     }
 
-
-    if( optable[op] != 0x36 )
-	cmdbuf[3] = optable[op];
+    if (optable[op] != 0x36)
+        cmdbuf[3] = optable[op];
     else {
-	memcpy(cmdbuf,chmbuf,10);
-	cmdbuf[5] = op - 16;
+        memcpy(cmdbuf, chmbuf, 10);
+        cmdbuf[5] = op - 16;
     }
     cmdbuf[2] += SLOT;
 
     unsigned char ix, cks = 0;
-    for( ix = 0; ix < cmdbuf[4]+4; ix++ )
-	cks += cmdbuf[ix];
+    for (ix = 0; ix < cmdbuf[4] + 4; ix++)
+        cks += cmdbuf[ix];
     cmdbuf[ix] = cks;
 
-    FILE *fp = fopen( "/dev/ttyUSB0", "wb" );
-    fwrite( cmdbuf,cmdbuf[4]+6,1,fp );
+    FILE *fp = fopen("/dev/ttyUSB0", "wb");
+    fwrite(cmdbuf, cmdbuf[4] + 6, 1, fp);
     fclose(fp);
     return 0;
 }
